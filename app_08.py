3b0f9562ca7318d28bfcd94e6c7715f35a595186
omele4ka <omele4ka@inbox.ru>
2023-12-16 14:04:04 -0500

    Task 3 is done

--- app_08.py
+++ app_08.py
@@ -4,3 +4,23 @@
 # При нажатии на кнопку будет произведено
 # перенаправление на страницу с flash сообщением, где будет
 # выведено "Привет, {имя}!".
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+
+
+app = Flask(__name__)
+
+
+app.secret_key = b'eb2ebbee6952bbbf87f20f2c7a12264a0d6aaa75d85a5d57516b56ee26998ba3' 
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    username = request.form.get('username')
+    if request.method == 'POST':
+        flash(f'Привет, {username}', 'success')
+        return redirect(url_for('hello'))
+    return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
